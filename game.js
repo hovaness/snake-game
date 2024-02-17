07e6d8ed4c7d9bdc5c53b47787bb1b1700e2a49e
Prizeox <oganisyanararat1@gmail.com>
2024-02-17 15:50:54 +0400

    final version

--- game.js
+++ game.js
@@ -1,13 +1,15 @@
 const BLOCK_SIZE = 15; // ширина и высота клетки в пикселях
 const FIELD_WIDTH = 20; // ширина игрового поля в клетках(гор.)
 const FIELD_HEIGHT = 17; // ширина игрового поля в клетках(верт.)
-const SNAKE_COLOR = "aqua";
-const APPLE_COLOR = "green";
-let DIRECTION = null;
-let SPEED = 500;
+const SNAKE_COLOR = "aquamarine";
+const SNAKE_HEAD = 'aqua'
+const APPLE_COLOR = "red";
+let DIRECTION = 'Right';
 
+const score = document.getElementById("score");
 const canvas = document.getElementById("gameField");
 const context = canvas.getContext("2d");
+
 const snake = [
     { x: 4, y: 2 },
     { x: 3, y: 2 },
@@ -15,6 +17,9 @@ const snake = [
     { x: 1, y: 2 },
 ];
 const apple = { x: null, y: null, score: 0 }
+let SPEED = 200;
+
+let game = setInterval(moveSnake,SPEED);
 
 function drawBlock(xPos, yPos, color) {
     context.fillStyle = color;
@@ -47,7 +52,7 @@ function drawApple() {
     if (!isOccupied(x, y)) {
         apple.x = x
         apple.y = y
-        drawBlock(apple.x, apple.y, APPLE_COLOR);
+        drawBlock(apple.x,apple.y,APPLE_COLOR)
     } else drawApple();
 }
 
@@ -58,9 +63,17 @@ function isOccupied(x, y) {
     return false;
 }
 
+function changeSpeed(){
+    SPEED > 50 ? SPEED -= Math.floor(Math.log(apple.score +1) * 3) : SPEED
+    clearInterval(game)
+    game = setInterval(moveSnake, SPEED)
+}
+
 function isOccupiedApple(x, y) {
     if (x == apple.x && y == apple.y) {
         apple.score += 1;
+        changeSpeed()
+        score.innerText = `Score: ${apple.score}`;
         drawApple()
         return true;
     }
@@ -69,6 +82,7 @@ function isOccupiedApple(x, y) {
 
 function gameOver() {
     alert(`Game over your score is ${apple.score}`)
+    clearInterval(game);
 }
 
 function outOfBorder(x, y) {
@@ -76,15 +90,26 @@ function outOfBorder(x, y) {
     else return false;
 }
 
+document.addEventListener("keydown", getDirection);
+
+function getDirection(event) {
+	if(event.keyCode == 37 && DIRECTION != "Right")
+		DIRECTION = "Left";
+	else if(event.keyCode == 38 && DIRECTION != "Down")
+		DIRECTION = "Up";
+	else if(event.keyCode == 39 && DIRECTION != "Left")
+		DIRECTION = "Right";
+	else if(event.keyCode == 40 && DIRECTION != "Up")
+		DIRECTION = "Down";
+}
 
 
 drawSnake();
-drawApple();
+drawApple()
+appleImg.onload =  game;
 
-document.addEventListener("keydown", moveSnake);
 
-function moveSnake(dir) {
-    DIRECTION = dir.code.substring(5);
+function moveSnake() {
     let { x, y } = snake[0];
     let isApple = isOccupiedApple(x, y);
 
@@ -136,3 +161,4 @@ function moveSnake(dir) {
             break;
     }
 }
+
